31892b04f3ddcbe970d8940bbedf0310aa862244
daniss <daniscin@gmail.com>
2025-11-09 21:46:42 +0100

    add modified app project and the architecture image

---
+++ web-app/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import * as session from 'express-session';
+import { randomBytes } from 'crypto';
+import { AppSessionBaseType } from './libs/data-structures/app-session.type';
+
+declare module 'express-session' {
+  export interface SessionData extends AppSessionBaseType {}
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Express session middleware setup
+  app.use(session({
+    secret: randomBytes(42).toString('hex'),
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, 'views'));
+  app.setViewEngine('hbs');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+}
+bootstrap();
